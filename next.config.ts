4aac0e2577769dca4f53dd9ffa4316cb44b2c8fe
saranskumar <saranskumarwh@gmail.com>
2026-08-09 23:20:20 +0530

    fix: set basePath conditionally for production build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/Procomm-26",
+  basePath: isProd ? "/Procomm-26" : "",
 
   images: {
     unoptimized: true,
